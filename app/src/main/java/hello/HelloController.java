fe02a9a23a86621be895c8b4839e2aae7eab7e2e
yjw2288 <yjw2288@coupang.com>
2016-04-14 13:51:16 +0900

    프록시 테스트를 위한 랜덤 숫자 나오는 컨트롤러 구성

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
+    private static final int RANDOM = (int) (Math.random() * 100);
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
@@ -14,5 +16,10 @@ public class HelloController {
     @RequestMapping("/ok")
     public String hello() {
         return "ok";
-    }   
+    }
+
+    @RequestMapping("/random")
+    public String random() {
+        return "random : " + RANDOM;
+    }
 }
